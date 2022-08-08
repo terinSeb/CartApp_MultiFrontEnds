d8224b4d4335dd4caf65d0e1ab7d24c62f49561d
Terin Sebastin <terin01@gmail.com>
2022-08-08 07:47:08 +0100

    Created container deploy script

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,2 +1,12 @@
-import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+
+export default () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        mount(ref.current);
+    });
+
+    return <div ref={ref} />;
+};
