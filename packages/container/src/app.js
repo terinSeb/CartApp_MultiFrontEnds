a97b1854aecc899f72071e663e819b708ca00c00
Terin Sebastin <terin01@gmail.com>
2022-08-15 01:36:09 +0100

    Corrected css error

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+});
 export default () => {
     return (
         <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}></StylesProvider>
             <div>
                 <Header />
                 <MarketingApp />
             </div>
+            <BrowserRouter />
         </BrowserRouter>
     );
 };
