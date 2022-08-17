6866c9c8ea8350392a6483c18be2189464a69e68
Terin Sebastin <terin01@gmail.com>
2022-08-17 01:17:11 +0100

    Added auth page

---
+++ packages/auth/src/app.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+// import Landing from './components/Landing';
+// import Pricing from './components/Pricing';
+import SignUp from './components/Signup';
+import SignIn from './components/Signin';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'au',
+});
+
+export default ({ history }) => {
+    return (
+        <div>
+            <StylesProvider generateClassName={generateClassName}>
+                <Router history={history}>
+                    <Switch>
+                        <Route path='auth/signin' component={SignIn}></Route>
+                        <Route path='auth/signup' component={SignUp}></Route>
+                    </Switch>
+                </Router>
+            </StylesProvider>
+        </div>
+    );
+};
