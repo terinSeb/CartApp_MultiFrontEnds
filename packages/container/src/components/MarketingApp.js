dc2bdc3a5badbd2681da8b4f640b2861546466db
Terin Sebastin <terin01@gmail.com>
2022-08-21 10:12:56 +0100

    Added auth subapp

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,20 +1,25 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+
 export default () => {
     const ref = useRef(null);
     const history = useHistory();
+
     useEffect(() => {
-        const { onParentNavigation } = mount(ref.current, {
+        const { onParentNavigate } = mount(ref.current, {
             initialPath: history.location.pathname,
             onNavigate: ({ pathname: nextPathname }) => {
                 const { pathname } = history.location;
-                if (pathname !== nextPathname)
+
+                if (pathname !== nextPathname) {
                     history.push(nextPathname);
-            }
+                }
+            },
         });
-        history.listen(onParentNavigation);
-    });
+
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref} />;
 };
