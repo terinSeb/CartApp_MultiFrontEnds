618395b758f31ab10f570167f8b90d8cbbcf8462
Terin Sebastin <terin01@gmail.com>
2022-08-22 03:22:55 +0100

    Added auth Dashboard

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -9,7 +9,7 @@ const mount = (el) => {
 };
 
 // If we are in development and in isolation,
-// call mount immediately
+// call mount immediately -- comment
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_dashboard-dev-root');
 
